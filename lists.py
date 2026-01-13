24373c1f3bd4cd83170d72e2cb277c2155104d63
Nanditha586 <nandithavemula@gmail.com>
2026-01-13 11:48:22 +0530

    lists are added

---
+++ lists.py
@@ -0,0 +1,12 @@
+numbers=[23,54,12,33,1]
+names=['raju','ravi','raghu']
+mixed=['raghav',1,0.8,True]
+print(numbers)
+print(names)
+print(mixed)
+for i in numbers:
+    print(i)
+if 'ravi' in names:
+    print("ravi is found in names")
+matrix=[[1,2,3],[4,5,6]]
+print(names.reverse())
