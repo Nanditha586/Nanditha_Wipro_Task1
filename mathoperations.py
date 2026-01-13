5406f3511c0468522ef9c9369b49779bb37367e2
Nanditha586 <nandithavemula@gmail.com>
2026-01-13 10:49:19 +0530

    2 files are added

---
+++ mathoperations.py
@@ -0,0 +1,4 @@
+import math
+print(math.sqrt(36))
+print(math.log(100))
+print(math.exp(100))
---
+++ strings.py
@@ -0,0 +1,11 @@
+a='hello'
+print(a)
+print(a[1])
+print(a[-5])
+print(a[1:4])
+
+b="""hello
+world
+python"""
+print(b)
+print(b[2])
