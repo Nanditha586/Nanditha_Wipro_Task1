8763cc2846232685043689c3b28c67c38778964a
Nanditha586 <nandithavemula@gmail.com>
2026-01-20 10:26:03 +0530

    match file added

---
+++ Day_6 20Jan2026/Class_Programs/match.py
@@ -0,0 +1,9 @@
+import re
+text="python is powerful language"
+result=re.match("python",text)
+if result:
+    print("match found")
+else:
+    print("no match found")
+searchresult=re.search("python",text)
+print(searchresult.group())
